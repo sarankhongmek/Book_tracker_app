ba09da3ab90e9856cb0d6be536a3fdd45c759ae7
Saran-Khongmek <James.warrender1@btinternet.com>
2018-10-23 10:55:13 +0100

    rm -rf node_modules

--- components/Searchbooks.js
+++ components/Searchbooks.js
@@ -7,18 +7,36 @@ class Search extends Component {
 
     render() {
         return (
+
+
             <div className="search-books">
+
+
                 <div className="search-books-bar">
-                    <a
-                        className="close-search"
-                        onClick={() => this.setState({showSearchPage: false})}>Close</a>
+
+
+                    <a className="close-search" onClick={() => this.setState({showSearchPage: false})}>Close</a>
+
+
                     <div className="search-books-input-wrapper">
+
                         <input type="text" placeholder="Search by title or author"/>
+
                     </div>
+
+
                 </div>
+
+
                 <div className="search-books-results">
+
+
                     <ol className="books-grid"></ol>
+
+
                 </div>
+
+                
             </div >
         )
     }
