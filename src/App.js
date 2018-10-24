11e84e7c8f0d3e985d8d64f66eaecc85def85d45
Saran-Khongmek <James.warrender1@btinternet.com>
2018-10-24 11:10:21 +0100

    rendering external data from booksAPI

--- src/App.js
+++ src/App.js
@@ -17,7 +17,11 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: false,
+    books:[],
+    currentReading:[],
+    wantToRead:[],
+    read:[]
   }
 
   render() {
--- src/components/BookCase.js
+++ src/components/BookCase.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react'
 import BookShelf from "./BookShelf";
 import { Link } from 'react-router-dom'
+import * as BooksAPI from '../BooksAPI'
 
 class BookCase extends Component  { 
-    state ={}
+    state ={
+        books:[],
+        currentReading:[],
+        wantToRead:[],
+        read:[],
+        addBooks: books => {}
+    }
     
+//getall books from api request
+     componentDidMount(){
+        BooksAPI.getAll().then((books) => {
+            this.setState({ books })
+            //console.log(books)
+        })
+        const books = BooksAPIgetAll();
+        const current = books.filter(book => book.shelf === 'currentReading');
+        const want = books.filter(book => book.shelf === 'wantToRead');
+        const read = books.filter(book => book.shelf === 'read');
+        console.log(current, want ,read);
+    }
+
+
     render () {
-        let shelves = [];
+        
 
         return (
 
             //main app
-            
+            //List-boots-content contain book shelves of 3 different shelves name
             <div className="app">
 
                
@@ -29,7 +50,7 @@ class BookCase extends Component  {
                     <div className="list-books-content">
 
                         <div>
-                          
+                    
                             <BookShelf title='Currently Reading'/>
                             <BookShelf title='Want To Read'/>
                             <BookShelf title='Read'/>
