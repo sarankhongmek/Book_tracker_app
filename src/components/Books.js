f4f0a00f844d7839398e40d5fb7331e08b83834c
Saran-Khongmek <James.warrender1@btinternet.com>
2018-11-03 14:10:54 +0000

    Update readme.md and fixed warning errors

--- src/components/Books.js
+++ src/components/Books.js
@@ -10,7 +10,7 @@ class Book extends Component {
         return (
             
 
-            <li>
+            
 
 
                 <div className="book">
@@ -47,7 +47,7 @@ class Book extends Component {
                 </div>
 
 
-            </li>
+            
 
 
         )
